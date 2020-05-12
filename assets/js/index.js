5f54e060493f0833dfac2dac88db552d3e0fac40
biplab-nag <biplab.diu@gmail.com>
2020-05-12 19:29:56 +0600

    intro added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
+	// navbar
 	$('.navbar').waypoint(function() {
 		$('.navbar').addClass('animate__animated animate__fadeInDown')
+	},{
+		offset: '50%'
+	});
+
+	// about
+	$('.about').waypoint(function() {
+		$('.about').addClass('animate__animated animate__fadeInUp')
 	},{
 		offset: '50%'
 	});
